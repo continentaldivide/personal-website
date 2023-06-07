be6e76e4d47d167c7380b96822de5f643067b07d
Andrew <andrew@andrewsmith.org>
2023-06-07 01:53:16 -0400

    Develop styling

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
-    <>
+    <div className="container w-2/5 my-auto">
       <p>this is contact</p>
-    </>
+    </div>
   );
 }
--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -1,7 +1,7 @@
 export default function Portfolio() {
   return (
-    <>
+    <div className="container w-2/5 my-auto">
       <p>this is portfolio</p>
-    </>
+    </div>
   );
 }
