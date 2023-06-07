31cb427f0e6fe610fdff274a8080f42b775b202d
Andrew <andrew@andrewsmith.org>
2023-06-07 02:12:21 -0400

    Add contact content

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,7 +1,39 @@
 export default function Contact() {
   return (
-    <div className="container w-2/5 my-auto">
-      <p>this is contact</p>
+    <div className="container w-1/2 my-auto">
+      <h1 className="text-5xl font-bold leading-relaxed">Contact me</h1>
+      <h2 className="text-3xl font-light leading-relaxed">
+        Please reach out -- I'd love to hear from you!
+      </h2>
+      <br />
+      <ul>
+        <li className="text-xl font-light leading-relaxed">
+          Email me at{' '}
+          <a href="mailto:andrew@andrewsmith.org" className="underline">
+            andrew@andrewsmith.org
+          </a>
+        </li>
+        <li className="text-xl font-light leading-relaxed">
+          Connect with me on{' '}
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/andrewsmithunc/"
+            className="underline"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li className="text-xl font-light leading-relaxed">
+          View my work on{' '}
+          <a
+            target="_blank"
+            href="https://github.com/continentaldivide/"
+            className="underline"
+          >
+            GitHub
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
