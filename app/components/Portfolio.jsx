b03ea5f76af66e03fc06a2bfa706534edecc5357
andrew <andrew@andrewsmith.org>
2023-06-28 15:03:28 -0400

    Redesign + refactor portfolio page

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -1,4 +1,42 @@
+import PortfolioItem from "./PortfolioItem";
+
 export default function Portfolio() {
+  const PortfolioData = [
+    {
+      title: "calientes",
+      projectLink: "https://calientes-nine.vercel.app/",
+      gitHubLink: "https://github.com/continentaldivide/calientes/",
+      imageUrl: "https://placekitten.com/400/300/",
+      text: "Calientes is an app focused towards fans of the YouTube series Hot Ones. It uses a custom backend data API to enable users to browse various statistics and fun facts about the series.",
+    },
+    {
+      title: "nearmint",
+      projectLink: "https://nearmint.app/",
+      gitHubLink: "https://github.com/continentaldivide/nearmint/",
+      imageUrl: "https://placekitten.com/400/300/",
+      text: "Nearmint is an app that leverages the Marvel comics API to offer users the opportunity to catalog their collection of Marvel comics,along with curating a wishlist of comics they'd like to get.",
+    },
+    {
+      title: "hectic harvest",
+      projectLink: "https://hecticharvest.com/",
+      gitHubLink: "https://github.com/continentaldivide/hectic-harvest/",
+      imageUrl: "https://placekitten.com/400/300/",
+      text: "Hectic Harvest is a fast-paced farming game inspired by cozy farming sims like Harvest Moon and Stardew Valley. It was built with HTML Canvas.",
+    },
+  ];
+
+  const portfolioItems = PortfolioData.map((portfolioItem, i) => {
+    return (
+      <PortfolioItem
+        title={portfolioItem.title}
+        projectLink={portfolioItem.projectLink}
+        gitHubLink={portfolioItem.gitHubLink}
+        imageUrl={portfolioItem.imageUrl}
+        text={portfolioItem.text}
+        key={`project ${i}`}
+      />
+    );
+  });
   return (
     <div className="container w-1/2 mt-20">
       <h1 className="text-5xl font-bold leading-relaxed">Recent projects</h1>
@@ -6,80 +44,7 @@ export default function Portfolio() {
         (Management apologizes for the temporary placeholder images)
       </h2>
       <br />
-      <div className="flex flex-wrap justify-between gap-4">
-        <div className="basis-1/2">
-          <p className="text-2xl font-bold leading-relaxed">
-            <a
-              target="_blank"
-              href="https://calientes-nine.vercel.app/"
-              className="underline hover:brightness-200"
-            >
-              calientes
-            </a>
-          </p>
-          <p className="text-xl leading-relaxed">
-            Calientes is an app focused towards fans of the YouTube series Hot
-            Ones. It uses a custom backend data API to enable users to browse
-            various statistics and fun facts about the series.
-          </p>
-        </div>
-        <div className="basis-2/5">
-          <a
-            target="_blank"
-            href="https://calientes-nine.vercel.app/"
-          >
-            <img src="https://placekitten.com/400/200"></img>
-          </a>
-        </div>
-        <div className="basis-1/2">
-          <p className="text-2xl font-bold leading-relaxed">
-            <a
-              target="_blank"
-              href="https://nearmint.app/"
-              className="underline hover:brightness-200"
-            >
-              nearmint
-            </a>
-          </p>
-          <p className="text-xl leading-relaxed">
-            Nearmint is an app that leverages the Marvel comics API to offer
-            users the opportunity to catalog their collection of Marvel comics,
-            along with curating a wishlist of comics they'd like to get.
-          </p>
-        </div>
-        <div className="basis-2/5">
-          <a
-            target="_blank"
-            href="https://nearmint.app/"
-          >
-            <img src="https://placekitten.com/400/200"></img>
-          </a>
-        </div>
-        <div className="basis-1/2">
-          <p className="text-2xl font-bold leading-relaxed">
-            <a
-              target="_blank"
-              href="https://hecticharvest.com/"
-              className="underline hover:brightness-200"
-            >
-              hectic harvest
-            </a>
-          </p>
-          <p className="text-xl leading-relaxed">
-            Hectic Harvest is a fast-paced farming game inspired by cozy farming
-            sims like Harvest Moon and Stardew Valley. It was built with HTML
-            Canvas.
-          </p>
-        </div>
-        <div className="basis-2/5">
-          <a
-            target="_blank"
-            href="https://hecticharvest.com/"
-          >
-            <img src="https://placekitten.com/400/200"></img>
-          </a>
-        </div>
-      </div>
+      {portfolioItems}
     </div>
   );
 }
---
+++ app/components/PortfolioItem.jsx
@@ -0,0 +1,34 @@
+export default function PortfolioItem({
+  title,
+  projectLink,
+  gitHubLink,
+  imageUrl,
+  text,
+}) {
+  return (
+    <div className="mb-4">
+      <div className="flex justify-between">
+        <p className="text-3xl font-bold leading-relaxed space-x-2">
+          <a
+            target="_blank"
+            href={projectLink}
+            className="underline hover:brightness-200"
+          >
+            {title}
+          </a>
+        </p>
+        <a target="_blank" href={gitHubLink} className="hover:brightness-200">
+          <img
+            height="32"
+            width="32"
+            src="https://cdn.simpleicons.org/github/CCCCCC/"
+          />
+        </a>
+      </div>
+      <a target="_blank" href={projectLink}>
+        <img src={imageUrl}></img>
+      </a>
+      <p className="text-xl leading-relaxed">{text}</p>
+    </div>
+  );
+}
